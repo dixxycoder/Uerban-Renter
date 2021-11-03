45674feb29919151944ca6b8d395b016d18aafb6
Grant Spraker <grant.spraker@gmail.com>
2021-11-03 10:46:08 -0600

    got some of the labels and buttons on. lot to do.

---
+++ Small_Tools_Controller.swift
@@ -0,0 +1,29 @@
+//
+//  Small_Tools_Controller.swift
+//  Uerban Renter
+//
+//  Created by Grant Spraker on 11/3/21.
+//
+
+import UIKit
+
+class Small_Tools_Controller: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
